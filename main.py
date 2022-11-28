f502121a53abf3f93a33819d6971f5247bf3b8a1
sanaas246 <112659421+sanaas246@users.noreply.github.com>
2022-11-28 14:44:18 -0700

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from pygame.locals import (
     K_DOWN,
     K_LEFT,
     K_RIGHT,
+    K_SPACE,
     K_ESCAPE,
     KEYDOWN,
     QUIT,
@@ -23,20 +24,22 @@ m = 1
 class Enemy(pygame.sprite.Sprite):
     def __init__(self):
         super(Enemy, self).__init__()
-        self.surf = pygame.Surface((20, 10))
+        self.surf = pygame.Surface((50, random.randint(100, 400)))
         self.surf.fill((255, 255, 255))
         self.rect = self.surf.get_rect(
             center=(
                 random.randint(SCREEN_WIDTH + 20, SCREEN_WIDTH + 100),
-                random.randint(0, SCREEN_HEIGHT),
+                550,
             )
         )
-        self.speed = random.randint(5, 10)
+        self.speed = 0.1
 
     def update(self):
-        self.rect.move_ip(-self.speed, 0)
+        self.rect.x -= self.speed
         if self.rect.right < 0:
             self.kill()
+        if self.rect.left == 0:
+            self.kill()    
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
@@ -46,33 +49,27 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.surf.get_rect()
         print(self.rect.x, self.rect.y)
 
-        self.isjumping = True
-        self.isfalling = False
-
     def gravity(self):
         if self.rect.top != 550:
             self.rect.y += 1
 
     def jump(self):
+        dy = -3
+        accel = 1
+        dy += accel
         self.isjumping = True
-
-        if self.isjumping is True:
-            self.rect.move_ip(0,-2)
-            pygame.time.wait(300)
-            self.rect.move_ip(0,2)
+        if self.isjumping == True:
+            self.rect.y += dy
             self.isjumping = False
-
 
     def update(self, pressed_keys):
         self.gravity()
-        if pressed_keys[K_UP]:
+        if pressed_keys[K_UP] or pressed_keys[K_SPACE]:
             self.jump()
-        if pressed_keys[K_DOWN]:
-            self.rect.move_ip(0, 2)
         if pressed_keys[K_LEFT]:
-            self.rect.move_ip(-2, 0)
+            self.rect.move_ip(-1, 0)
         if pressed_keys[K_RIGHT]:
-            self.rect.move_ip(2, 0)
+            self.rect.move_ip(1, 0)
 
             # Keep player on the screen
         if self.rect.left < 0:
@@ -89,7 +86,14 @@ pygame.init()
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+ADDENEMY = pygame.USEREVENT + 1
+pygame.time.set_timer(ADDENEMY, 250)
+
 player = Player()
+enemies = pygame.sprite.Group()
+all_sprites = pygame.sprite.Group()
+all_sprites.add(player)
+
 
 running = True
 
@@ -101,11 +105,22 @@ while running:
         elif event.type == QUIT:
             running = False
 
+        elif event.type == ADDENEMY:
+            # Create the new enemy and add it to sprite groups
+            new_enemy = Enemy()
+            enemies.add(new_enemy)
+            all_sprites.add(new_enemy)
+
     pressed_keys = pygame.key.get_pressed()
     player.update(pressed_keys)
 
+    enemies.update()
+
     # Fill the screen with black
     screen.fill((70,70,70)) 
+
+    for entity in all_sprites:
+        screen.blit(entity.surf, entity.rect)
 
     pygame.draw.rect(screen, [100, 100, 100], [0, 550, 800, 100],0)
 
